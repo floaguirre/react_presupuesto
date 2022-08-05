3a44d7cb8738bb507417ae687420172b94dbf260
Flo Aguirre <aguirreflomik@gmail.com>
2022-08-05 10:24:22 -0300

    validacion de formulario

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -1,31 +1,46 @@
-
+import { useState } from "react";
+import Mensaje from "./Mensaje";
 
 const NewBudget = ({presupuesto, setPresupuesto}) => {
-  return (
-    <div className="contenedor-presupuesto contenedor sombra">
-        <form className="formulario">
-            <div className="campo">
-                <label>Definir Presupuesto</label>
+    
+    const [mensaje, setMensaje] = useState('')
+    
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje('No es un presupuesto válido')
+        }
+
+    }
+
+
+    return (
+        <div className="contenedor-presupuesto contenedor sombra">
+            <form onSubmit={handlePresupuesto} className="formulario">
+                <div className="campo">
+                    <label>Definir Presupuesto</label>
+                    <input
+                        className="nuevo-presupuesto"
+                        type="text"
+                        placeholder="Añade tu Presupuesto"
+                        value={presupuesto}
+                        onChange={(e) => setPresupuesto(e.target.value)}
+                    
+                    />
+
+
+                </div>
                 <input
-                    className="nuevo-presupuesto"
-                    type="text"
-                    placeholder="Añade tu Presupuesto"
-                    value={presupuesto}
-                    onChange={(e) => setPresupuesto(e.target.value)}
-                
+                    type="submit"
+                    value="Añadir" 
                 />
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
 
-
-            </div>
-            <input
-                type="submit"
-                value="Añadir" 
-            />
-
-        </form>
-      
-    </div>
-  )
+            </form>
+        
+        </div>
+    )
 }
 
 export default NewBudget
